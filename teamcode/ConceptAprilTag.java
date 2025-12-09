3ed3484f87b6743e721b26f6671053f67ec6fa81
Oliver Beresford <oggynaut@icloud.com>
2025-12-09 16:34:42 -0500

    Add position and orientation telemetry for AprilTag detection

--- teamcode/ConceptAprilTag.java
+++ teamcode/ConceptAprilTag.java
@@ -4,6 +4,9 @@ import android.util.Size;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -149,6 +152,13 @@ public class ConceptAprilTag extends LinearOpMode {
                 telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                 telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                 telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                
+                // Add position on the field
+                Position robotPosition = detection.robotPose.getPosition();
+                YawPitchRollAngles robotOrientation = detection.robotPose.getOrientation();
+                telemetry.addLine(String.format("\n==== (ID %d) %s, Robot Position:", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", robotPosition.x, robotPosition.y, robotPosition.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", robotOrientation.getPitch(), robotOrientation.getRoll(), robotOrientation.getYaw()));
             } else {
                 telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
                 telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));

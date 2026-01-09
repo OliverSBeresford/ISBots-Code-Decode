e7318aead5ce1f203e516963d59f8012b4353c34
Oliver Beresford <oggynaut@icloud.com>
2026-01-09 13:27:27 -0500

    Add initial implementation of AutoAim class for AprilTag detection

---
+++ teamcode/Autonomous.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
+
+@Autonomous(name="Autonomous (Blue Tag 20)")
+public class AutoAim extends OpMode {
+
+    private static final int BLUE_BASKET_TAG_ID = 20;   // change to 24 for red
+    private static final double FALLBACK_RPM = 3200;    // used if tag not visible
+
+    private RobotUtils robot = null;
+
+    @Override
+    public void init() {
+        robot = new RobotUtils(hardwareMap);
+        telemetry.addLine("Robot Ready.");
+        telemetry.addLine("AutoAim: uses AprilTag ID 20 (blue basket).");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+    }
+}
